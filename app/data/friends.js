63aaca8136f985ff8ee65b44c6d79c1adf0e0aaa
Lucent <lquint85@gmail.com>
2019-02-21 14:52:41 -0600

    modified friends.js

---
+++ app/data/friends.js
@@ -0,0 +1,89 @@
+let profileLog = [{
+        name: "Superman",
+        photo: "https://media.licdn.com/dms/image/C4E03AQEvRaqHN5PGxQ/profile-displayphoto-shrink_200_200/0?e=1548892800&v=beta&t=j_LaEkLw9dt8mbZhRSBH0Xr8jDY2eEIZfENi6MZhRJs",
+        scores: [
+            5,1,4,4, 5,5,2,5,4,5
+        ]
+    },
+    {
+        name: "Batman",
+        photo: "https://media.licdn.com/dms/image/C4E03AQEvRaqHN5PGxQ/profile-displayphoto-shrink_200_200/0?e=1548892800&v=beta&t=j_LaEkLw9dt8mbZhRSBH0Xr8jDY2eEIZfENi6MZhRJs",
+        scores: [
+            5,1,3,3, 5,3,2,3,4,1
+        ]
+   
+    },
+    {
+        name: "Robin",
+        photo: "https://media.licdn.com/dms/image/C4E03AQEvRaqHN5PGxQ/profile-displayphoto-shrink_200_200/0?e=1548892800&v=beta&t=j_LaEkLw9dt8mbZhRSBH0Xr8jDY2eEIZfENi6MZhRJs",
+        scores: [
+            5,2,4,2, 5,2,2,5,2,2
+        ]
+    },
+    {
+        name: "Joker",
+        photo: "https://media.licdn.com/dms/image/C4E03AQEvRaqHN5PGxQ/profile-displayphoto-shrink_200_200/0?e=1548892800&v=beta&t=j_LaEkLw9dt8mbZhRSBH0Xr8jDY2eEIZfENi6MZhRJs",
+        scores: [
+            5,1,4,1, 5,1,1,5,1,1
+        ]
+    },
+    {
+        name: "Bane",
+        photo: "https://media.licdn.com/dms/image/C4E03AQEvRaqHN5PGxQ/profile-displayphoto-shrink_200_200/0?e=1548892800&v=beta&t=j_LaEkLw9dt8mbZhRSBH0Xr8jDY2eEIZfENi6MZhRJs",
+        scores: [
+            5,1,2,4, 5,2,2,5,4,1
+        ]
+    },
+    {
+        name: "Penguin",
+        photo: "https://media.licdn.com/dms/image/C4E03AQEvRaqHN5PGxQ/profile-displayphoto-shrink_200_200/0?e=1548892800&v=beta&t=j_LaEkLw9dt8mbZhRSBH0Xr8jDY2eEIZfENi6MZhRJs",
+        scores: [
+            5,1,4,4, 5,1,2,5,4,4
+        ]
+    },
+    {
+        name: "Batgirl",
+        photo: "https://media.licdn.com/dms/image/C4E03AQEvRaqHN5PGxQ/profile-displayphoto-shrink_200_200/0?e=1548892800&v=beta&t=j_LaEkLw9dt8mbZhRSBH0Xr8jDY2eEIZfENi6MZhRJs",
+        scores: [
+            5,1,4,4, 4,1,4,5,4,1
+        ]
+    },
+    {
+        name: "Alfred",
+        photo: "https://media.licdn.com/dms/image/C4E03AQEvRaqHN5PGxQ/profile-displayphoto-shrink_200_200/0?e=1548892800&v=beta&t=j_LaEkLw9dt8mbZhRSBH0Xr8jDY2eEIZfENi6MZhRJs",
+        scores: [
+            5,1,4,5,5 ,5,2,5,4,5
+        ]
+    },
+    {
+        name: "Two-face",
+        photo: "https://media.licdn.com/dms/image/C4E03AQEvRaqHN5PGxQ/profile-displayphoto-shrink_200_200/0?e=1548892800&v=beta&t=j_LaEkLw9dt8mbZhRSBH0Xr8jDY2eEIZfENi6MZhRJs",
+        scores: [
+            5,4,4,4, 5,4,4,5,4,4
+        ]
+    },
+    {
+        name: "Poisin Ivy",
+        photo: "https://media.licdn.com/dms/image/C4E03AQEvRaqHN5PGxQ/profile-displayphoto-shrink_200_200/0?e=1548892800&v=beta&t=j_LaEkLw9dt8mbZhRSBH0Xr8jDY2eEIZfENi6MZhRJs",
+        scores: [
+            5,1,4,3, 5,1,2,3,3,3
+        ]
+    },
+    {
+        name: "Scarecrow",
+        photo: "https://media.licdn.com/dms/image/C4E03AQEvRaqHN5PGxQ/profile-displayphoto-shrink_200_200/0?e=1548892800&v=beta&t=j_LaEkLw9dt8mbZhRSBH0Xr8jDY2eEIZfENi6MZhRJs",
+        scores: [
+            5,3,3,4, 5,3,2,5,4,3
+        ]
+    },
+    {
+        name: "KnightWing",
+        photo: "https://media.licdn.com/dms/image/C4E03AQEvRaqHN5PGxQ/profile-displayphoto-shrink_200_200/0?e=1548892800&v=beta&t=j_LaEkLw9dt8mbZhRSBH0Xr8jDY2eEIZfENi6MZhRJs",
+        scores: [
+            5,1,4,3, 5,3,2,3,4,1
+        ]
+    }
+
+];
+
+module.exports = profileLog;
