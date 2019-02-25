d1a54606827e22ab21ff939389aa8f37310000f3
Lucent <lquint85@gmail.com>
2019-02-24 19:48:51 -0600

    added pics

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,6 +1,6 @@
 let profileLog = [{
         name: "Superman",
-        photo: "https://media.licdn.com/dms/image/C4E03AQEvRaqHN5PGxQ/profile-displayphoto-shrink_200_200/0?e=1548892800&v=beta&t=j_LaEkLw9dt8mbZhRSBH0Xr8jDY2eEIZfENi6MZhRJs",
+        photo: "https://vignette.wikia.nocookie.net/superman/images/b/b1/Superman_Action_976_Gary_Frank.png/revision/latest?cb=20170501140424",
         scores: [
             5,1,4,4, 5,5,2,5,4,5
         ]
@@ -15,70 +15,70 @@ let profileLog = [{
     },
     {
         name: "Robin",
-        photo: "https://media.licdn.com/dms/image/C4E03AQEvRaqHN5PGxQ/profile-displayphoto-shrink_200_200/0?e=1548892800&v=beta&t=j_LaEkLw9dt8mbZhRSBH0Xr8jDY2eEIZfENi6MZhRJs",
+        photo: "https://upload.wikimedia.org/wikipedia/en/thumb/d/de/Batman_and_Robin_Eternal_1_Variant.png/250px-Batman_and_Robin_Eternal_1_Variant.png",
         scores: [
             5,2,4,2, 5,2,2,5,2,2
         ]
     },
     {
         name: "Joker",
-        photo: "https://media.licdn.com/dms/image/C4E03AQEvRaqHN5PGxQ/profile-displayphoto-shrink_200_200/0?e=1548892800&v=beta&t=j_LaEkLw9dt8mbZhRSBH0Xr8jDY2eEIZfENi6MZhRJs",
+        photo: "https://pixel.nymag.com/imgs/daily/vulture/2016/08/05/05-skirllex-joker.w330.h330.jpg",
         scores: [
             5,1,4,1, 5,1,1,5,1,1
         ]
     },
     {
         name: "Bane",
-        photo: "https://media.licdn.com/dms/image/C4E03AQEvRaqHN5PGxQ/profile-displayphoto-shrink_200_200/0?e=1548892800&v=beta&t=j_LaEkLw9dt8mbZhRSBH0Xr8jDY2eEIZfENi6MZhRJs",
+        photo: "https://upload.wikimedia.org/wikipedia/en/thumb/e/e1/Bane_Batman_Vol_3_18.png/250px-Bane_Batman_Vol_3_18.png",
         scores: [
             5,1,2,4, 5,2,2,5,4,1
         ]
     },
     {
         name: "Penguin",
-        photo: "https://media.licdn.com/dms/image/C4E03AQEvRaqHN5PGxQ/profile-displayphoto-shrink_200_200/0?e=1548892800&v=beta&t=j_LaEkLw9dt8mbZhRSBH0Xr8jDY2eEIZfENi6MZhRJs",
+        photo: "https://cdn.shopify.com/s/files/1/1141/1726/products/7af1a27703b54fd0b6b36e0bd6dbc5ablg_800x.jpg?v=1534148725",
         scores: [
             5,1,4,4, 5,1,2,5,4,4
         ]
     },
     {
         name: "Batgirl",
-        photo: "https://media.licdn.com/dms/image/C4E03AQEvRaqHN5PGxQ/profile-displayphoto-shrink_200_200/0?e=1548892800&v=beta&t=j_LaEkLw9dt8mbZhRSBH0Xr8jDY2eEIZfENi6MZhRJs",
+        photo: "http://cdn.shopify.com/s/files/1/0054/4371/5170/products/FiGPiN_86_Batgirl_BATMAN_CLASSIC_PIN_grande.png?v=1539210330",
         scores: [
             5,1,4,4, 4,1,4,5,4,1
         ]
     },
     {
         name: "Alfred",
-        photo: "https://media.licdn.com/dms/image/C4E03AQEvRaqHN5PGxQ/profile-displayphoto-shrink_200_200/0?e=1548892800&v=beta&t=j_LaEkLw9dt8mbZhRSBH0Xr8jDY2eEIZfENi6MZhRJs",
+        photo: "https://statici.behindthevoiceactors.com/behindthevoiceactors/_img/chars/alfred-pennyworth-batman-the-animated-series-12.6.jpg",
         scores: [
             5,1,4,5,5 ,5,2,5,4,5
         ]
     },
     {
         name: "Two-face",
-        photo: "https://upload.wikimedia.org/wikipedia/en/8/87/Batman_DC_Comics.png",
+        photo: "https://i.pinimg.com/236x/b7/69/5f/b7695f1a84bc47b441259df2eb62dec0--printmaking-ideas-two-faces.jpg",
         scores: [
             5,4,4,4, 5,4,4,5,4,4
         ]
     },
     {
         name: "Poisin Ivy",
-        photo: "https://media.licdn.com/dms/image/C4E03AQEvRaqHN5PGxQ/profile-displayphoto-shrink_200_200/0?e=1548892800&v=beta&t=j_LaEkLw9dt8mbZhRSBH0Xr8jDY2eEIZfENi6MZhRJs",
+        photo: "https://i.pinimg.com/originals/fd/2b/0d/fd2b0d936a39256b4873ab128e22b0fb.png",
         scores: [
             5,1,4,3, 5,1,2,3,3,3
         ]
     },
     {
         name: "Scarecrow",
-        photo: "https://media.licdn.com/dms/image/C4E03AQEvRaqHN5PGxQ/profile-displayphoto-shrink_200_200/0?e=1548892800&v=beta&t=j_LaEkLw9dt8mbZhRSBH0Xr8jDY2eEIZfENi6MZhRJs",
+        photo: "https://i.pinimg.com/originals/7e/4b/9c/7e4b9c4be60008d1885bb9f7a964d8e3.png",
         scores: [
             5,3,3,4, 5,3,2,5,4,3
         ]
     },
     {
         name: "KnightWing",
-        photo: "https://media.licdn.com/dms/image/C4E03AQEvRaqHN5PGxQ/profile-displayphoto-shrink_200_200/0?e=1548892800&v=beta&t=j_LaEkLw9dt8mbZhRSBH0Xr8jDY2eEIZfENi6MZhRJs",
+        photo: "https://vignette.wikia.nocookie.net/dccomicsfanon/images/d/d3/Knightwing.jpg/revision/latest?cb=20180815134855",
         scores: [
             5,1,4,3, 5,3,2,3,4,1
         ]
